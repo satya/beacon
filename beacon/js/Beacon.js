322419a7418fa566c8f9967319f077a6f5299576
Sunil K Chopra <dayer4b@gmail.com>
2013-05-01 09:49:58 -0500

    added more filename replacements for special characters

--- beacon/js/Beacon.js
+++ beacon/js/Beacon.js
@@ -295,7 +295,7 @@ Beacon.prototype.newDoc = function() {
         id = 0,
         container = "";
 
-    filename = filename.replace(/ /g, "_");
+    filename = filename.replace(/ /g, "_").replace(/[$-\/:-?{-~!"^`\[\]#@]/g,"_");
 
     if (filetype === "-1") {
         BeaconMessage.init(this.strings.messages["noFileType"]);
